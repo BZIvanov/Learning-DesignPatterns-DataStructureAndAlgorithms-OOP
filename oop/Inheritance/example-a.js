f145ac97003a058376da75447ada096ca8716a10
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-19 15:06:13 +0300

    update inheritance example

--- oop/Inheritance/example-a.js
+++ oop/Inheritance/example-a.js
@@ -11,6 +11,8 @@ function Square(width, height) {
 
 // with this line we will inherit from the Shape's prototype, not the base Object
 Square.prototype = Object.create(Shape.prototype);
+// when we reset the prototype we should also reset the constructor
+Square.prototype.constructor = Square;
 
 Square.prototype.squaresPrint = function () {
   console.log('I am square');
