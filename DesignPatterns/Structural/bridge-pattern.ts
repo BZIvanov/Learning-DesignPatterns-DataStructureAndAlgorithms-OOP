619e6a97d5754c3e33779f700e4004a6d65bb48b
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-21 19:58:09 +0300

    bridge pattern

---
+++ DesignPatterns/Structural/bridge-pattern.ts
@@ -0,0 +1,39 @@
+interface IPaymentProcessor {
+  processPayment(payment: string): void;
+}
+
+class CashPaymentProcessor {
+  processPayment(payment: string) {
+    console.log('Processing cash payment...');
+    console.log(payment);
+    console.log('Payment processed!');
+  }
+}
+
+class OnlinePaymentProcessor {
+  processPayment(payment: string) {
+    console.log('Processing online payment...');
+    console.log(payment);
+    console.log('payment processed!');
+  }
+}
+
+class User {
+  name: string;
+  private paymentProcessor: IPaymentProcessor;
+
+  constructor(name, paymentProcessor: IPaymentProcessor) {
+    this.name = name;
+    this.paymentProcessor = paymentProcessor;
+  }
+
+  processPayment(product: string) {
+    console.log(`Processing payment for ${this.name}`);
+    this.paymentProcessor.processPayment(product);
+  }
+}
+
+const user1 = new User('Iva', new CashPaymentProcessor());
+user1.processPayment('Shoes');
+const user2 = new User('Kiril', new OnlinePaymentProcessor());
+user2.processPayment('Book');
