a9636cc478c986eafaa936e97fe03e1a4ff283a0
BZIvanov <biser.z.ivanov@gmail.com>
2021-10-19 22:51:58 +0300

    basic events demo handling events

---
+++ microservices/basic-events-demo/moderation/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('banana') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:5005/events', {
+      type: 'CommentModerated',
+      data: { id: data.id, postId: data.postId, status, content: data.content },
+    });
+  }
+
+  res.status(201).json({ success: true });
+});
+
+const PORT = process.env.PORT || 5003;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
