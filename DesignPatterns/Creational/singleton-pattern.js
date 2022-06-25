761bbe1a8858c4c51ebb26e7741644b41e38e92e
BZIvanov <biser.z.ivanov@gmail.com>
2022-06-25 12:02:58 +0300

    singleton pattern more examples

--- DesignPatterns/Creational/singleton-pattern.js
+++ DesignPatterns/Creational/singleton-pattern.js
@@ -1,3 +1,4 @@
+// example 1 with Class
 class Theme {
   constructor() {
     if (Theme.instance instanceof Theme) {
@@ -24,3 +25,43 @@ const secondInstance = new Theme();
 
 console.log(firstInstance.get('opacity'));
 console.log(secondInstance.get('opacity'));
+
+console.log('----------');
+
+// example 2 with Class
+
+class AppC {
+  constructor() {}
+
+  run() {
+    console.log('App running');
+  }
+
+  stop() {
+    console.log('App stopped');
+  }
+}
+
+const myApp = new AppC();
+Object.freeze(myApp);
+myApp.run();
+myApp.stop();
+console.log(myApp);
+
+console.log('----------');
+
+// example with Object literal
+
+const AppO = {
+  run: () => console.log('AppO running'),
+  stop: () => console.log('AppO stopped'),
+};
+
+Object.freeze(AppO);
+
+AppO.run();
+AppO.stop();
+
+AppO.run = 'Test';
+AppO.stop = 5;
+console.log('Our App is not changed: ', AppO);
